a7d07ef56861b3565f83a8de8b8eef62755e17ca
Amer <amerlopo5@gmail.com>
2025-09-28 23:39:34 +0200

    .

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
 import express from "express";
 import cors from "cors";
-import routes from "./routes/index"; // prilagodi path
+import routes from "./routes/index";
 import { logger } from "./middleware/logger.middleware";
 
 const app = express();
@@ -9,12 +10,11 @@ app.use(cors());
 app.use(express.json());
 app.use(logger);
 
-// Sve tvoje rute
 app.use("/api", routes);
 
 app.get("/", (_req, res) => {
   res.json({ message: "Bibliobus backend is running!" });
 });
 
-// Eksportuješ Express app kao serverless handler
-export default app;
+// Ovo je ključ: eksportujemo **handler** umjesto app.listen
+export default (req: VercelRequest, res: VercelResponse) => app(req, res);
