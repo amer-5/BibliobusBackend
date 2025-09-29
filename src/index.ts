323c3520e9b8d3228a1480a3b4629d8600957e6e
Amer <amerlopo5@gmail.com>
2025-09-29 21:04:50 +0200

    .

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 import express from "express";
 import cors from "cors";
-import routes from "./routes/index"; // sve tvoje rute
+import routes from "./routes"; // sve tvoje rute
 import { logger } from "./middleware/logger.middleware";
 
 const app = express();
@@ -10,5 +10,8 @@ app.use(cors());
 app.use(express.json());
 app.use(logger);
 app.use("/", routes);
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
 
 export default (req: VercelRequest, res: VercelResponse) => app(req, res);
